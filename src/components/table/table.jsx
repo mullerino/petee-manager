0532d520a72c8dba962f9eeea74429424fcd021a
Leandro <leandromllr9@gmail.com>
2024-02-11 17:17:23 -0300

    refactor: table component

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -1,86 +1,25 @@
-import React from 'react';
-import { Space, Table, Tag } from 'antd';
+import { Spin, Table } from 'antd';
 
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
-  {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: (_, { tags }) => (
-      <>
-        {tags.map((tag) => {
-          let color = tag.length > 5 ? 'geekblue' : 'green';
-          if (tag === 'loser') {
-            color = 'volcano';
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          );
-        })}
-      </>
-    ),
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: (_, record) => (
-      <Space size="middle">
-        <a>Invite {record.name}</a>
-        <a>Delete</a>
-      </Space>
-    ),
-  },
-];
+import styles from './table.module.css'
 
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser'],
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sydney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
-  },
-];
-
-const Table = () => {
+const TableGeneric = ({ columns, data, isLoading }) => {
   return (
-    <>
-      <Table columns={columns} dataSource={data} />
-    </>
+    <div className={styles.table}>
+      <Table
+        columns={columns}
+        dataSource={data}
+        loading={{
+          indicator: <Spin />,
+          spinning: isLoading
+        }}
+        pagination={{
+          defaultPageSize: 6,
+          showSizeChanger: true,
+          pageSizeOptions: ['6', '10', '15']
+        }}
+      />
+    </div>
   )
 }
 
-
-export default Table;
+export default TableGeneric;
