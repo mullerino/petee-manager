1bdab8734460375c11111b9bf549f7a5e5d60b51
Leandro <leandromllr9@gmail.com>
2024-02-12 13:30:33 -0300

    feat: implementing functionality to add petiano and notification on the page

--- src/components/content/content.jsx
+++ src/components/content/content.jsx
@@ -1,18 +1,41 @@
 import TableGeneric from '../table/table'
 import styles from './content.module.css'
-
+import { useMutation } from 'react-query'
 import { Tooltip, Button } from 'antd';
 import { PlusOutlined } from '@ant-design/icons'
 import Forms from '../forms/forms';
 import { useModalContext } from '../../contexts/modalForm';
+import { registerNewPetiano } from '../../fetchs';
+import { useEffect } from 'react';
+import { useQueryClient } from '../../contexts/useQuery';
+import { useNotification } from '../../contexts/notification';
+
 
 const Content = ({ data, columns, isLoading, tooltipAdding, fields }) => {
   const { openModal, addNewRegister, closeModal } = useModalContext();
+  const { queryClientInstance } = useQueryClient();
+  const { mutate, isSuccess, isError} = useMutation(registerNewPetiano, {
+    onSuccess: () => {
+      queryClientInstance.invalidateQueries('petianos');
+    }
+  });
+  const { openNotification } = useNotification();
 
-  const onCreate = (values) => {
-    console.log('Received values of form: ', values);
-    closeModal()
+  const onCreate = (dataFromForm) => {
+    console.log('Received values of form: ', dataFromForm);
+    mutate(dataFromForm);
+    closeModal();
   };
+
+  useEffect(() => {
+    if (isSuccess || isError) {
+      const notificationTitle = isSuccess ? 'Deu tudo certo' : 'Algo deu errado';
+      const notificationMessage = isSuccess ? 'As informações inseridas no formulário foram submetidas.' : 'As informações inseridas no formulário não foram submetidas.';
+      
+      openNotification(notificationTitle, notificationMessage, isSuccess);
+    }
+
+  }, [isSuccess, isError])
 
   return (
     <div className={styles.container}>
@@ -35,6 +58,8 @@ const Content = ({ data, columns, isLoading, tooltipAdding, fields }) => {
             closeModal()
           }}
           fields={fields}
+          errorSubmit={isError}
+          sucessSubmit={isSuccess}
         />
       </div>
       <div>
