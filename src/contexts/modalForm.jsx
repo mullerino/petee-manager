51b42761a2445965b92244889500f350bc1aae67
Leandro <leandromllr9@gmail.com>
2024-02-16 13:21:11 -0300

    feat: adjusting the modal context to create a state that stores the record id

--- src/contexts/modalForm.jsx
+++ src/contexts/modalForm.jsx
@@ -6,21 +6,23 @@ export const useModalContext = () => useContext(ModalContext);
 
 export const ModalProvider = ({ children }) => {
   const [openModal, setOpenModal] = useState({ isOpen: false, editMode: false });
+  const [idEditRegister, setIdEditRegister] = useState();
 
   const addNewRegister = () => {
-    setOpenModal({ isOpen: true, editMode: false })
+    setOpenModal({ isOpen: true, editMode: false });
   }
 
-  const editRegister = () => {
-    setOpenModal({ isOpen: true, editMode: true })
+  const editRegister = (id) => {
+    setOpenModal({ isOpen: true, editMode: true });
+    setIdEditRegister(id);
   }
 
   const closeModal = () => {
-    setOpenModal({ isOpen: false, editMode: false })
+    setOpenModal({ isOpen: false, editMode: false });
   }
 
   return (
-    <ModalContext.Provider value={{ openModal, addNewRegister, editRegister, closeModal }}>
+    <ModalContext.Provider value={{ openModal, idEditRegister, addNewRegister, editRegister, closeModal }}>
       {children}
     </ModalContext.Provider>
   )
