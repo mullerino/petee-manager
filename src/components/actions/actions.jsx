0907b0544aefef117f5cac1ce338413061afa191
Leandro <leandromllr9@gmail.com>
2024-02-16 13:15:43 -0300

    feat: adding the functionality to edit a record

--- src/components/actions/actions.jsx
+++ src/components/actions/actions.jsx
@@ -2,30 +2,50 @@ import { Space, Tooltip, Popconfirm } from 'antd';
 import { EditOutlined, DeleteOutlined, QuestionCircleOutlined } from '@ant-design/icons'
 import { useModalContext } from '../../contexts/modalForm';
 import { useMutation } from 'react-query'
-import { deletePetiano } from '../../fetchs';
 import { useQueryClient } from '../../contexts/useQuery';
 import { useNotification } from '../../contexts/notification';
+import { useFormContext } from '../../contexts/form';
 
-const Actions = ({ record }) => {
+const Actions = ({ record, deleteTypeRegister, keyQuery, fieldsForms, data }) => {
   const { editRegister } = useModalContext();
   const { queryClientInstance } = useQueryClient();
   const { openNotification } = useNotification();
-  const { mutate } = useMutation(deletePetiano, {
+  const form = useFormContext();
+
+  const { mutate } = useMutation(deleteTypeRegister, {
     onSuccess: (response) => {
       openNotification('Deu tudo certo', `${response.message}!`, true);
-      queryClientInstance.invalidateQueries('petianos');
+      queryClientInstance.invalidateQueries(keyQuery);
     }
   }
   );
 
-  const onDeletePetiano = (id) => {
+  const setFieldsValuesFromRecord = (fields, selectedRegister) => {
+    const values = {};
+    fields.forEach(field => {
+      if (selectedRegister.hasOwnProperty(field.name)) {
+        values[field.name] = selectedRegister[field.name];
+      }
+    });
+    return values;
+  };
+
+  const onEditRegister = () => {
+    const selectedRecord = data.filter((row) => row.id === record.key)[0];
+    const initialValuesRegister = setFieldsValuesFromRecord(fieldsForms, selectedRecord);
+
+    form.setFieldsValue(initialValuesRegister);
+    editRegister(selectedRecord.id);
+  };
+
+  const onDeleteRegister = (id) => {
     mutate(id)
   };
 
   return (
     <Space size="middle">
       <Tooltip placement='bottom' title="Editar">
-        <a><EditOutlined onClick={() => editRegister()} /></a>
+        <a><EditOutlined onClick={() => onEditRegister()} /></a>
       </Tooltip>
       <Tooltip placement='bottom' title="Deletar">
         <a>
@@ -45,7 +65,7 @@ const Actions = ({ record }) => {
               danger: true
             }}
             onConfirm={() => {
-              onDeletePetiano(record.key)
+              onDeleteRegister(record.key)
             }}
           >
             <DeleteOutlined />
