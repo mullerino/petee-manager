327001b8ab2809f7e9ae0f6f1ba8e7f5e65074a5
Leandro <leandromllr9@gmail.com>
2024-02-16 13:14:39 -0300

    feat: creating the context for the form

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import RoutesApp from './routes';
 import { ModalProvider } from './contexts/modalForm';
 import { QueryClientContextProvider } from './contexts/useQuery';
 import { NotificationProvider } from './contexts/notification';
+import { FormProvider } from './contexts/form';
 
 function App() {
   return (
     <QueryClientContextProvider>
-      <NotificationProvider>
-        <ModalProvider>
-          <RoutesApp />
-        </ModalProvider>
-      </NotificationProvider>
+      <FormProvider>
+        <NotificationProvider>
+          <ModalProvider>
+            <RoutesApp />
+          </ModalProvider>
+        </NotificationProvider>
+      </FormProvider>
     </QueryClientContextProvider>
   )
 }
